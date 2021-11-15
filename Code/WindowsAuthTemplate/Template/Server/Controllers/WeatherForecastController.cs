c344e131122acfea1ae84b523b87c7817eb58441
Jeff Lefebvre <thestamp@gmail.com>
2021-11-14 22:50:57 -0600

    added windows auth

--- Code/WindowsAuthTemplate/Template/Server/Controllers/WeatherForecastController.cs
+++ Code/WindowsAuthTemplate/Template/Server/Controllers/WeatherForecastController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Template.Server.Models;
 
 namespace Template.Server.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
