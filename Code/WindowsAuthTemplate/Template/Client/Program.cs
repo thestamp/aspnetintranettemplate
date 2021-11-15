4ad596196a412f3f6376dfce8625c0143c94872d
Jeff Lefebvre <thestamp@gmail.com>
2021-11-15 00:48:24 -0600

    SOA added (#3)
    * added service handlers for weatherforecast
    
    * updated readme
    
    * updated readme (again)
    
    * Update README.md

--- Code/WindowsAuthTemplate/Template/Client/Program.cs
+++ Code/WindowsAuthTemplate/Template/Client/Program.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ServiceClients;
 using Template.Client;
+using Template.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new UserServerApiClient(builder.HostEnvironment.BaseAddress, new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }));
+
+//because the WeatherForecastService constructor parameters are previously DI'd objects, DI can handle the parameters for us.
+builder.Services.AddScoped<WeatherForecastService>();
 
 await builder.Build().RunAsync();
---
+++ Code/WindowsAuthTemplate/Template/Client/Services/WeatherForecastService.cs
@@ -0,0 +1,22 @@
+﻿using ServiceClients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Client.Services
+{
+    public class WeatherForecastService
+    {
+        private UserServerApiClient _apiClient;
+        public WeatherForecastService(UserServerApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+        public async Task<ICollection<WeatherForecast>> GetWeatherForecasts()
+        {
+            return await _apiClient.GetAsync();
+        }
+    }
+}
